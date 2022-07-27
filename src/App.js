aa8f518e2870cc1b50191c1c46ae49e209bb90ec
SStuurman <stuart@lateral.solutions>
2022-07-28 00:18:16 +0200

    designed the home pade layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import ExerciseDetails from './pages/ExerciseDetails';
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <Box width="400px" sx={{ width: {xl: '1488px' }}} m="auto">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/exercise/:id" element={<ExerciseDetails/>}/>
+        </Routes>
+        <Footer />
+      </Box>
     </div>
   );
 }
